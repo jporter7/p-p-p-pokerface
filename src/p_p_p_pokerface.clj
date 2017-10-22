d23566c31a5fe6ca0ca77097df78c6a700a4ec3e
jporter7@ycp.edu <jporter7@ycp.edu@kec119-12>
2017-10-22 16:36:47 -0400

    finished?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,61 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [val (get card 0)]
+     (if (Character/isDigit val)
+       (Integer/valueOf (str val))
+       (get {\T 10 \J 11 \Q 12 \K 13 \A 14} val))))
 
 (defn suit [card]
-  nil)
+  (str (get card 1)))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)
+         freq (frequencies ranks)
+         val (vals freq)
+         check (fn [x] (== x 2))]
+     (not (empty? (filter check val)))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)
+         freq (frequencies ranks)
+         val (vals freq)
+         check (fn [x] (== x 3))]
+     (not (empty? (filter check val)))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)
+         freq (frequencies ranks)
+         val (vals freq)
+         check (fn [x] (== x 4))]
+     (not (empty? (filter check val)))))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+     (apply = suits)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (four-of-a-kind? hand) (= (count (filter even? (vals (frequencies (map rank hand))))) 2)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+         rankslow (sort (replace {14 1} ranks))
+         low (apply min ranks)
+         high (apply max ranks)
+         str (range low (+ low 5))
+         str2 (range 1 6)]
+     (or (= ranks str) (= rankslow str2))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[pair? 1]
+                  [two-pairs? 2]  [three-of-a-kind? 3]
+                  [straight? 4]   [flush? 5]
+                  [full-house? 6] [four-of-a-kind? 7]
+                  [straight-flush? 8]}]
+   (apply max (map (fn [x] (if ((first x) hand) (second x) 0)) checkers))))
